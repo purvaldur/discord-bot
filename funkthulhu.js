b8b8f80428b860dc8f7a4a2fa7dc98a713d7762e
purvaldur <thorvaldha@gmail.com>
2016-10-25 00:45:16 +0200

    added songsearch funtionality to play individual tracks one by one.

--- funkthulhu.js
+++ funkthulhu.js
@@ -4,8 +4,10 @@ var streams = require('./streams.js');
 var request = require('request')
 var underscore = require('underscore');
 var icecast = require('icecast');
+var PlayMusic = require('playmusic');
 
 var mybot = new Discord.Client();
+var pm = new PlayMusic();
 var voice_channel;
 var stationlist_string;
 var dispatcher;
@@ -50,7 +52,7 @@ mybot.on("message", function(message) {
             radio_playing = false;
  		}
 	}
-	if(message.content.indexOf("!radio ") != -1 && message.content !== "!radio info" && message.content !== "!radio list") {
+	if(message.content.startsWith("!radio ") && message.content !== "!radio info" && message.content !== "!radio list") {
 	    voice_channel = message.member.voiceChannel;
 		var radio_string = message.content.replace("!radio ", "");
 		radio = streams.streamlist[radio_string];
@@ -104,6 +106,28 @@ mybot.on("message", function(message) {
 		message.channel.sendMessage("The following is a list of the currently available stations:\n\n");
 		radiolist();
 		message.channel.sendMessage(stationlist_string);
+	}
+	if (message.content.startsWith("!songsearch ")) {
+        var songsearch_string = message.content.replace("!songsearch ", "");
+		if (!voice_channel || voice_channel.type !== 'voice') {
+			message.channel.sendMessage("Error! (are you not in a voice channel?)");
+		} else {
+			if (mybot.voiceConnections.get(message.guild.id) && mybot.voiceConnections.get(message.guild.id).channel.id == voice_channel.id) {
+                pm.init({androidId: android_id, masterToken: android_masterToken}, function(err) {
+                    pm.search(songsearch_string, 5, function(err, res) {
+                        var gSong = res.entries.filter(function(data) {
+                                return data.type == 1
+                        }).shift();
+                        pm.getStreamUrl(gSong.track.nid, function(err, streamUrl) {
+            				dispatcher = mybot.voiceConnections.get(message.guild.id).playStream(request(streamUrl), {seek:0, volume:0.1});
+            				mybot.user.setStatus("Online", "funky tunes!");
+                        });
+                    });
+                });
+			} else {
+				message.channel.sendMessage("Error! (are you not in the same voice channel as I am?)");
+			}
+		}
 	}
 
 	if (message.content === "!restart") {
