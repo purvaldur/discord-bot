8d6bae1c1b3af6e74f925a969c5787a5e5f9e9bb
purvaldur <thorvaldha@gmail.com>
2016-10-27 19:44:50 +0200

    Added "!help" command. Prints a list of commands

---
+++ test.js
@@ -0,0 +1,14 @@
+var q = {};
+
+if (!q.something) {
+    q.something = []
+    console.log(q.something.length);
+}
+
+
+
+//things = JSON.parse(`{ "author": "110406708299329536", "playlist": {} }`);
+//things.playlist["track"] = "123";
+//things = JSON.stringify(things)
+////things.playlist.push('"track": "123"');
+//console.log(things);
