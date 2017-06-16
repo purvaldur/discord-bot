94107a5f3e006e50570b85a78c6bf4739fada36e
root <root@vps361513.ovh.net>
2017-06-16 23:40:22 +0200

    .gitignore is now working

--- tokens.js
+++
@@ -1,7 +0,0 @@
-bot_token = 'MTc0NTM1NzExNjMyOTE2NDgw.C1QknQ.3GciowZSwBvVnB1SABQu96Iq8D0'; //funkthulhu
-testbot_token = 'MjQxMTUwMjA3MDY3MjkxNjQ4.C3CR8g.Ge6R-H8LdJR7uGRh4K8iYjN1wkw'; //test bot
-testbot_token2 = 'MjQ4MDE2Mjc2MjQ3Njc0ODgw.C3CoYQ.LGDR-s8tldLMOeUEyv4mqbivAr8'; //webthlhu/ajax
-toastbot_token = 'MTEwNDA2NzA4Mjk5MzI5NTM2.C5JagA.NRZoDjE5Ch99SEW_6Ha9jNVhfO4ø'; //self bot toast
-lomzbot_token = 'OTM3NDQ0MTUzMDE5NzE5Njg.CwR22A.kXH5ODPpp-ucYQrCxJoB__DZXjc'; //self bot lomz
-android_id = '357B18D4CED2C753';
-android_masterToken = 'oauth2rt_1/tjmG7LqJBS0HPi_4FJmG08WG3oJgFmxjgwknVMlLNJI';
